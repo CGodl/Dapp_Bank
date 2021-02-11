1c9f5e9a5cc93769b2e9b6410b9eb249dbe291e5
Carl Godlewski <cgodlew@gmail.com>
2021-02-10 23:57:59 -0500

    Added testing to check whether token has a name

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -6,7 +6,33 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('TokenFarm', (accounts) => {
-  
+  function tokens(n) {
+    return web3.utils.toWei(n, 'ether')
+  }
+
+contract('TokenFarm', ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm
+
+  before (async () => {
+
+    //Loading the contracts
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+    //Transfer all Dapp tokens to farm
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+       //Transfer all Dapp tokens to farm
+    await daiToken.transfer(investor, tokens('100'), {from: owner })
+  })
+
+  describe('Mock Dai Deployment', async () => {
+    it('has a name', async () => {
+      let daiToken = await DaiToken.new()
+      const name = await daiToken.name()
+      assert.equal(name, 'Mock DAI Token')
+    })
+  })
 
 })
