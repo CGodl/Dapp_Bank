ad9d27c482862f145b855631636bd68d3ab68e1f
Carl Godlewski <cgodlew@gmail.com>
2021-02-10 19:42:34 -0500

    Added transfer of 100 daiTokens to investor account. Transferred full amount of dappTokens to TokenFarm

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,26 @@
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm)
+
+
+module.exports = async function(deployer, network, accounts) {
+
+  // This sections deploys our DaiToken
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // This sections deploys our DappToken
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+
+  deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  //Transfers all dappTokens to the TokenFarm. 
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  //Transfers 100 daiTokens to investor (In this case investor will be the second account at index 1)
+  await daiToken.transfer(accounts[1], '100000000000000000000')
 }
