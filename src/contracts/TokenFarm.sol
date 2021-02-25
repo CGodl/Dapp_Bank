1d710f760e3bd06e328a68614554e4cb41097270
Carl Godlewski <cgodlew@gmail.com>
2021-02-25 13:10:02 -0500

    First bug commit

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -4,28 +4,32 @@ import "./DappToken.sol";
 import "./DaiToken.sol";
 
 contract TokenFarm {
-  string public name = "DApp Token Farm";
+  string public name = "Dapp Token Farm";
+  address public owner;
   DappToken public dappToken;
   DaiToken public daiToken;
-  address public owner;
-
+  
   address[] public stakers;
   mapping(address => uint) public stakingBalance;
   mapping(address => bool) public hasStaked;
   mapping(address => bool) public isStaking;
 
-  constructor(DappToken _dappToken, DaiToken _daiToken ) public {
+  constructor(DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
     owner = msg.sender;
   }
 
   function stakeTokens(uint _amount) public {
+
+    require(_amount > 0, "amount cannot be 0");
+
+
     daiToken.transferFrom(msg.sender, address(this), _amount); //This is the address of the smart contract/bank
 
     stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-    //add user to staking array only if they havent already staked.
+    //Add user to staking array only if they havent already staked.
     if(!hasStaked[msg.sender]) {
       stakers.push(msg.sender);
     }
@@ -35,44 +39,25 @@ contract TokenFarm {
     hasStaked[msg.sender] = true;
   }
 
-
-
   function unstakeTokens() public {
     uint balance = stakingBalance[msg.sender];
-
     require(balance > 0, 'staking balance cannot be 0');
-
     daiToken.transfer(msg.sender, balance);
-
     stakingBalance[msg.sender] = 0;
-
     isStaking[msg.sender] = false;
-
-
-
   }
-
-
-
-
-
-
 
   function issueTokens() public {
     //Only owner can call function
-    require(msg.sender == owner, 'Caller must be the owner');
-
+    require(msg.sender == owner, "caller must be the owner");
 
     //Issue token to all stakers
-    for (uint i=0; i < stakers.length; i++) {
+    for (uint i=0; i<stakers.length; i++) {
       address recipient = stakers[i];
       uint balance = stakingBalance[recipient];
-      if(balance > 0) {
+      if (balance > 0) {
         dappToken.transfer(recipient, balance);
       }
     }
   }
-
-  
-
 }
