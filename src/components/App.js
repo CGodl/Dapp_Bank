48b364d36bd984912a84644175adb763a511c09a
Carl Godlewski <cgodlew@gmail.com>
2021-03-01 21:50:50 -0500

    Set up function to load blockchain data from account

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,74 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
-import './App.css'
-import Web3 from 'web3'
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import './App.css';
+import Web3 from 'web3';
+import DaiToken from '../abis/DaiToken.json';
+import DappToken from '../abis/DappToken.json';
+import TokenFarm from '../abis/TokenFarm.json';
 
 class App extends Component {
 
+  async componentDidMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  };
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    this.setState({account: accounts[0]});
+
+    const networkId = await web3.eth.net.getId();
+
+
+    const daiTokenData = DaiToken.networks[networkId];
+    if (daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address);
+      this.setState({daiToken});
+      const daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call();
+      this.setState({daiTokenBalance: daiTokenBalance.toString()});
+
+    }
+    else {
+      window.alert("DaiToken Contract could not be applied to network");
+    };
+
+    const dappTokenData = DappToken.networks[networkId]
+    if (dappTokenData) {
+      const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address);
+      this.setState({dappToken});
+      const dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call();
+      this.setState({dappTokenBalance: dappTokenBalance.toString()});
+    }
+    else {
+      window.alert("DappToken Contract could not be applied to network");
+    }
+
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if (tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address);
+      this.setState({tokenFarm});
+      const stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call();
+      this.setState({stakingBalance: stakingBalance.toString()});
+    }
+    else {
+      window.alert("TokenFarm Contract could not be applied to network");
+    }
+
+    this.setState({loading: false})
+
+
+  }
+
   async loadWeb3() {
     if (window.ethereum) {
+      console.log('I hit window.ethereum')
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
     } 
     else if (window.web3) {
+      console.log('I hit window.web3')
       window.web3 = new Web3(window.web3.currentProvider)
     }
     else {
@@ -23,9 +81,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
     }
+    
   }
+
+
 
   render() {
     return (
