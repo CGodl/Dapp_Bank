a2063c7acbb5577449a6b699c22f25eac5582f9e
Carl Godlewski <cgodlew@gmail.com>
2021-03-08 17:35:06 -0500

    Added function to stakeTokens and unstakeTokens. Added form to stake tokens. added useRef hook to form for input

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,7 @@ class App extends Component {
     const web3 = window.web3;
 
     const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+
     this.setState({account: accounts[0]});
 
     const networkId = await web3.eth.net.getId();
@@ -73,7 +74,21 @@ class App extends Component {
     }
   }
 
+  stakeTokens = (amount) => {
+    this.setState({ loading: true })
+    this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
 
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true })
+    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
 
   constructor(props) {
     super(props)
@@ -103,8 +118,8 @@ class App extends Component {
         daiTokenBalance={this.state.daiTokenBalance}
         dappTokenBalance={this.state.dappTokenBalance}
         stakingBalance={this.state.stakingBalance}
-        // stakeToken={this.stakeToken}
-        // unstakeToken={this.unstakeToken}
+        stakeTokens={this.stakeTokens}
+        unstakeTokens={this.unstakeTokens}
       />
     }
 
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import dai from '../../dai.png';
 
 const Main = (props) => {
+  const numInput = useRef(null);
+
+  const stakeEvent = (e) => {
+    e.preventDefault();
+
+    let amount;
+    amount = numInput.current.value.toString();
+    amount = window.web3.utils.toWei(amount, 'Ether');
+    props.stakeTokens(amount)
+  }
+
+  const unstakeEvent = (e) => {
+    e.preventDefault();
+    props.unstakeTokens();
+  }
+
+
   return (
     <div id='content' className="mt-3">
 
@@ -21,9 +39,50 @@ const Main = (props) => {
 
       <div className='card mb-4'>
         <div className='card-body'>
-          Hello World
+        <form className="mb-3" onSubmit={e => stakeEvent(e)}
+          
+          // (event) => {
+          //       event.preventDefault()
+          //       let amount
+              
+          //       amount = numInput.current.value.toString()
+          //       amount = window.web3.utils.toWei(amount, 'Ether')
+          //       props.stakeTokens(amount)
+          //     }}
+              
+              >
+            <div>
+              <label className='float-left'><b>Stake Tokens</b></label>
+              <span className='float-right text-muted'>
+                Balance: {window.web3.utils.fromWei(props.daiTokenBalance, 'Ether')}
+              </span>
+            </div>
+            <div className='input-group mb-4'>
+              <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    ref={numInput}
+                    placeholder="0"
+                    required />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <img src={dai} height='32' alt=""/>
+                  &nbsp;&nbsp;&nbsp; mDAI
+                </div>
+              </div>
+            </div>
+            <button type='submit' className='btn btn-primary btn-block btn-lg'>Stake</button>              
+          </form>
+          <button 
+            type='submit'
+            className='btn btn-danger btn-block btn-lg'
+            onClick={(e) => unstakeEvent(e)}
+          >
+            Unstake
+          </button>
+              
         </div>
-
+          
       </div>
       
     </div>
